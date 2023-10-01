d835946aa3528afc082747402df8c5d86e373e0b
davidagina <davidagina64@gmail.com>
2023-10-01 21:19:25 +0100

    Book Service class added

---
+++ src/main/java/com/bookStore/service/BookService.java
@@ -0,0 +1,18 @@
+package com.bookStore.service;
+
+import com.bookStore.entity.Book;
+import com.bookStore.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bRepo;
+
+    public void save(Book b){
+        bRepo.save(b);
+    }
+
+}
