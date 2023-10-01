92a76195c0781f38d555072f510c52c581bbff4a
davidagina <davidagina64@gmail.com>
2023-10-01 21:20:14 +0100

    Updated

--- src/main/java/com/bookStore/controller/BookController.java
+++ src/main/java/com/bookStore/controller/BookController.java
@@ -11,4 +11,19 @@ public class BookController {
         return "home";
     }
 
+    @GetMapping("/book_register")
+    public String bookRegister(){
+        return "bookRegister";
+    }
+
+    @GetMapping("/available_book")
+    public String availableBook(){
+        return "availableBook";
+    }
+
+    @GetMapping("/my_books")
+    public String myBooks(){
+        return "myBooks";
+    }
+
 }
